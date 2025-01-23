7c688483dcc2037a390c089a3981be6ac3479511
Abu-Sayed-Sarker <abusayed96728@gmail.com>
2025-01-23 23:48:14 +0600

    remove blog body on home page

--- app/components/BlogsCard.jsx
+++ app/components/BlogsCard.jsx
@@ -1,14 +1,13 @@
 import Link from "next/link";
 
 const BlogsCard = ({ blog }) => {
-    const { id, title, body } = blog || {}
+    const { id, title } = blog || {}
     return (
 
         <Link href={`blogs/${id}`} className="card  bg-purple-400/10 hover:bg-purple-400/50 cursor-pointer">
 
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
-                <p>{body}</p>
             </div>
 
         </Link>
